e4f357024c9b310ad9a52fd25d292a8cbf39dbeb
Noitidart <noitidart@gmail.com>
2016-12-09 20:10:30 -0800

    rev9
    * InputNumber dragging

--- dom-react.js
+++ dom-react.js
@@ -2,6 +2,7 @@
 
 // react transition setuper
 // REQUIREMENTS:
+// dom.js - stopClickAndCheck0
 // var gTrans = [] setup on your side. `var` so i can access from here (dom-react.js)
 const ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
 function createTrans(transitionName, transitionEnterTimeout, transitionLeaveTimeout, transitionAppear=undefined) {
@@ -57,13 +58,73 @@ var InputNumber = React.createClass({
     console.log('defaultValue:', defaultValue);
     return {
       value: defaultValue,
-      dragging: false
+      dragging: 0 // 0-not dragging 1-dragging allow -1-dragging not allow
     }
   },
   onWheel(e) {
     this.crementBy(e.deltaY < 0 ? 0 : -0);
     e.stopPropagation();
     e.preventDefault();
+  },
+  onDragStart(e) {
+    // to be attached to onMouseDown
+    if (!stopClickAndCheck0(e)) return;
+
+    let { value, dragging } = this.state;
+    let { dragdir='vertical' } = this.props; // link34666
+    if (dragging) return; // already dragging, this should never happen
+    // TODO: check if it is a valid number? and return if not? this is what i did in my first version of InputNumber
+    let dimension = dragdir == 'vertical' ? 'clientY' : 'clientX';
+    this.drginfo = {
+      value_st: value,
+      dimension,
+      position_st: e[dimension]
+    };
+    console.log('setting into drag, drginfo:', this.drginfo);
+    this.setState({dragging:1}); // TODO: even if invalid value, dragging will set it to min or max
+  },
+  onDragStop(e) {
+    console.log('stopping drag');
+    this.setState({dragging:0});
+    delete this.drginfo;
+  },
+  onDrag(e) {
+    let { dimension, position_st, value_st } = this.drginfo;
+    let { sensitivty=10, crement=1, max, min } = this.props; // link199288 link266622
+    let { value, dragging } = this.state;
+    let position_delta = dimension.endsWith('Y') ? position_st - e[dimension] : e[dimension] - position_st; // so up/dn means increment/decrement and right/left means increment/decrement
+    let value_delta = Math.round(position_delta / sensitivty) * crement;
+    // console.log('position_delta:', position_delta, 'value_delta:', value_delta);
+    // TODO: if by_orig === 0 then maybe if over max, set to max, and if under min set to min?
+    let newvalue = value_st + value_delta;
+    // if (isTestMax() isTestOverMax()) == if (isTestMaxish())
+
+    let newdragging = 1;
+    if (this.isTestOverMax(newvalue)) {
+      newdragging = -1;
+      newvalue = max;
+    }
+    if (this.isTestUnderMin(newvalue)) {
+      newdragging = -1;
+      newvalue = min;
+    }
+    // // TODO: if invalid chars in newvalue - this method doesnt work
+    // if (!this.isTestValid(newvalue)) {
+    //   if (position_delta > 0) {
+    //     newvalue = max !== undefined ? max : 0;
+    //   } else {
+    //     newvalue = min !== undefined ? min : 0;
+    //   }
+    // }
+
+    // console.log('value vs newvalue', value, newvalue);
+    if (value !== newvalue || dragging !== newdragging) {
+      // this.crementTo(newvalue); // i dont use crementTo because i have to set dragging at the same time
+      this.setState({
+        dragging: newdragging,
+        value: newvalue
+      });
+    }
   },
   onTextChange(e) {
     this.setState({ value:e.target.value });
@@ -113,6 +174,7 @@ var InputNumber = React.createClass({
     // passed to component
     // change value by, can be positive or negative
     // pass -0 for -crement or just 0 for crement OR for auto correction from isNaN or isMinish or isMaxish
+    // passing 0 to `by` is special, as it will respect min/max if it has it
     let { value } = this.state;
     let { crement=1, min, max } = this.props; // link266622
     // if (isTestValid(value)) return false; // TODO: validation here?
@@ -165,37 +227,45 @@ var InputNumber = React.createClass({
     console.log('tested validity of value:', value, this.isTestValid(value));
     return this.isTestValid(value);
   },
-  isMinish() {
-    let { value } = this.state;
+  isTestMinish(num) {
     let { min } = this.props;
     if (min === undefined) return false;
-    if (isNaN(value)) return false;
-    return parseInt(value) <= min ? true : false;
-    // if (!this.isTestValid(value)) return false;
+    if (isNaN(num)) return false;
+    return parseInt(num) <= min ? true : false;
+  },
+  isMinish() {
+    let { value } = this.state;
+    return this.isTestMinish(value);
+  },
+  isTestUnderMin(num) {
+    let { min } = this.props;
+    if (min === undefined) return false;
+    if (isNaN(num)) return false;
+    return parseInt(num) < min ? true : false;
   },
   isUnderMin() {
     let { value } = this.state;
-    let { min } = this.props;
-    if (min === undefined) return false;
-    if (isNaN(value)) return false;
-    return parseInt(value) < min ? true : false;
-    // if (!this.isTestValid(value)) return false;
+    return this.isTestUnderMin(value);
+  },
+  isTestMaxish(num) {
+    let { max } = this.props;
+    if (max === undefined) return false;
+    if (isNaN(num)) return false;
+    return parseInt(num) >= max ? true : false;
   },
   isMaxish() {
     let { value } = this.state;
+    return this.isTestMaxish(value);
+  },
+  isTestOverMax(num) {
     let { max } = this.props;
     if (max === undefined) return false;
-    if (isNaN(value)) return false;
-    return parseInt(value) >= max ? true : false;
-    // if (!this.isTestValid(value)) return false;
+    if (isNaN(num)) return false;
+    return parseInt(num) > max ? true : false;
   },
   isOverMax() {
     let { value } = this.state;
-    let { max } = this.props;
-    if (max === undefined) return false;
-    if (isNaN(value)) return false;
-    return parseInt(value) > max ? true : false;
-    // if (!this.isTestValid(value)) return false;
+    return this.isTestOverMax(value);
   },
   componentWillUpdate(nextProps, nextState) {
     let { dispatcher } = this.props;
@@ -209,8 +279,8 @@ var InputNumber = React.createClass({
     let {
       component, // REQUIRED either just an input, with optionally a label and/or container
       // crement=1, // used at link266622 // size of increment/decrement
-      sensitivty=10, // min:1, pixels mouse should move before crementing
-      cursor='ew-resize', // cursor when mouse dragging can crementing
+      // sensitivty=10, // used at link199288 // min:1, pixels mouse should move before crementing
+      dragdir='vertical', // default also used at link34666 // cursor when mouse dragging can crementing
       min, // optional. min value
       max, // optinal. max value
       dispatcher, // optional - redux only - a function that takes one argument, like: `newval => store.dispatch(actionCreator(newval))`
@@ -218,7 +288,7 @@ var InputNumber = React.createClass({
       ...other // what the devuser wants to pass to the component
     } = this.props;
     // validation and mouseable always happens - but if no element to attach the mousedrag too then nothing happens. can set multiple elements as target for mousedrag
-    let { value } = this.state;
+    let { value, dragging } = this.state;
 
     if (!('maxlen' in this)) this.maxlen = max !== undefined ? max.toString().length : undefined;
 
@@ -229,12 +299,24 @@ var InputNumber = React.createClass({
       maxLength: this.maxlen,
       onWheel: this.onWheel
     };
-    let domprops_mouseable = { style:{cursor}, onWheel:this.onWheel };
+
+    let cursor = dragdir == 'vertical' ? 'ns-resize' : 'ew-resize';
+    let domprops_mouseable = { style:{cursor}, onWheel:this.onWheel, onMouseDown:this.onDragStart };
 
     let isvalid = this.isValid();
     let isminish = this.isMinish();
     let ismaxish = this.isMaxish();
+
+
+    let drag_cursor;
+    if (dragging) {
+      let { value_dragged } = this.drginfo;
+      drag_cursor = dragging === -1 ? 'not-allowed' : cursor;
+    }
+
     // component should set `wheelable#` `draggable#` or `allable#` - the input element will by default be wheelable
-    return React.createElement(component, { ref:'component', ...other, crementBy:this.crementBy, crementTo:this.crementTo, timedCrement:this.timedCrement, timedCrementStop:this.timedCrementStop, isvalid, ismaxish, isminish, domprops_text, domprops_mouseable });
+    return React.createElement(component, { ref:'component', ...other, crementBy:this.crementBy, crementTo:this.crementTo, timedCrement:this.timedCrement, timedCrementStop:this.timedCrementStop, isvalid, ismaxish, isminish, domprops_text, domprops_mouseable },
+      dragging ? React.createElement('div', { style:{zIndex:'2000', position:'fixed', top:'0', left:'0', width:'100vw', height:'100vh', cursor:drag_cursor}, onMouseUp:this.onDragStop, onMouseMove:this.onDrag }) : undefined // cant do the dragging && oterhwise that will print a 0 to dom // crossfile-link92828222
+    );
   }
 });
